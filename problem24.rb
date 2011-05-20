883f6b77c3067e6003802c249e54caa725eb3db6
Tom Fleischer <tfleischer@gmail.com>
2011-05-20 14:37:55 -0400

    Refactored Problem 24

--- problem24.rb
+++ problem24.rb
@@ -1,11 +1,5 @@
 class Problem24
   def Problem24.run
-    digits = (0..9).to_a
-    perm = digits.permutation(10).to_a[999_999]
-    result = ""
-    for d in perm
-      result += String(d)
-    end
-    puts result
+    puts (0..9).to_a.permutation(10).to_a[999_999].join
   end
 end
