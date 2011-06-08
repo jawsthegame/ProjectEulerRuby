67515cc1376b5150baadfa5f49df50960f675533
Tom Fleischer <tfleischer@gmail.com>
2011-06-08 12:50:11 -0400

    Reworked Problem 1

--- problem01.rb
+++ problem01.rb
@@ -1,18 +1,5 @@
 class Problem01
-  #If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-  #
-  #Find the sum of all the multiples of 3 or 5 below 1000.
-  #
-
   def Problem01.run
-    sum = 0
-
-    1000.times do |i|
-      if i % 3 == 0 || i % 5 == 0
-        sum += i
-      end
-    end
-
-    p sum
+    puts (1...1000).select{|n| n % 3 == 0 or n % 5 == 0}.inject(:+)
   end
 end
